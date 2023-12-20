f24c47fd0058d21f1c24be3afceaf5786e6f7fda
mahajar2 <rahul.mahajan2@mail.dcu.ie>
2023-12-20 20:30:35 +0000

    Removed images from being tracked

--- src/projects/homelab/homelab.js
+++ src/projects/homelab/homelab.js
@@ -1,10 +1,28 @@
 import './homelab.css';
+import React from 'react';
+import discordLogo from '../../images/discord.png';
+import Header from '../../header/header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHammer, faComments, faPlay, faPause, faForward, faList, faVolumeLow, faCirclePlay, faLayerGroup, faSquarePollVertical, faLanguage, faLink, faTrash, faBroom } from '@fortawesome/free-solid-svg-icons';
+import Footer from '../../footer/Footer';
 
 function Homelab() {
     return (
         <div>
-            <h1>Homelab</h1>
-        </div>
+            <Header />
+            <div className="discord-container">
+                <div className="logo-container">
+                    <img src={discordLogo} alt="Discord Logo" className="discord-logo" />
+                </div>
+                <div className="text-container">
+                    <h1 className="bot-title">Agni,<br /><span style={{ color: "white" }}>A Homelab Setup</span></h1>
+                </div>
+            </div>
+            <div className="additional-text two-paragraphs">
+
+            </div>
+            <Footer />
+        </div >
     )
 }
 
