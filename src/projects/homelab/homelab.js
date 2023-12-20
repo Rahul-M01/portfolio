8b0b671ae1b80ff294940b70bd4b34cf51358ff4
mahajar2 <rahul.mahajan2@mail.dcu.ie>
2023-12-20 21:00:32 +0000

    Added few bits and pieces to homelab page

--- src/projects/homelab/homelab.js
+++ src/projects/homelab/homelab.js
@@ -1,6 +1,6 @@
 import './homelab.css';
 import React from 'react';
-import discordLogo from '../../images/discord.png';
+import homelabLogo from '../../images/homelab.png';
 import Header from '../../header/header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHammer, faComments, faPlay, faPause, faForward, faList, faVolumeLow, faCirclePlay, faLayerGroup, faSquarePollVertical, faLanguage, faLink, faTrash, faBroom } from '@fortawesome/free-solid-svg-icons';
@@ -12,14 +12,30 @@ function Homelab() {
             <Header />
             <div className="discord-container">
                 <div className="logo-container">
-                    <img src={discordLogo} alt="Discord Logo" className="discord-logo" />
+                    <img src={homelabLogo} alt="Discord Logo" className="discord-logo" />
                 </div>
                 <div className="text-container">
-                    <h1 className="bot-title">Agni,<br /><span style={{ color: "white" }}>A Homelab Setup</span></h1>
+                    <h1 className="homelab-title">Agni,<br /><span style={{ color: "white" }}>A Homelab Setup</span></h1>
                 </div>
             </div>
             <div className="additional-text two-paragraphs">
-
+                <div className="paragraph">
+                    <ul>
+                        <li><FontAwesomeIcon icon={faHammer} />Prometheus</li>
+                    </ul>
+                </div>
+                <div className="paragraph">
+                    <ul>
+                        <li><FontAwesomeIcon icon={faCirclePlay} />Plex</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="additional-text two-paragraphs">
+                <div className="paragraph">
+                    <ul>
+                        <li><FontAwesomeIcon icon={faLayerGroup} />Home Assistant</li>
+                    </ul>
+                </div>
             </div>
             <Footer />
         </div >
