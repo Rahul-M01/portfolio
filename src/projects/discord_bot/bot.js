95d9c7ff9966d48ce9a28864afb191947f1d7eaa
mahajar2 <rahul.mahajan2@mail.dcu.ie>
2023-12-20 00:34:58 +0000

    Working on bot page

--- src/projects/discord_bot/bot.js
+++ src/projects/discord_bot/bot.js
@@ -1,23 +1,18 @@
-// Header.js
 import React from 'react';
 import './bot.css';
 import Header from '../../header/header';
 
 const Bot = () => {
-    // You would have state and functions here for handling mobile menu toggle
-
     return (
         <div>
             <Header />
-            <div className="discord-background">
-                <div class="discord-embed">
-                    <h2 class="embed-title">Embed Title</h2>
-                    <p class="embed-description">Here's the description of the embed, where the main content goes.</p>
-                    <div class="embed-footer">Embed Footer Text</div>
+            <div className="text-reveal-container">
+                <div className="text-part-one">Bhima:</div>
+                <div className="text-part-two">
+                    <span>A discord bot</span>
                 </div>
             </div>
         </div>
-
     );
 };
 
