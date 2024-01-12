8b9f48bdcfeea5a97ec194d9d98e546603a65a12
mahajar2 <rahul.mahajan2@mail.dcu.ie>
2024-01-12 16:33:34 +0000

    Fixed header and footer for mobile version

--- src/header/header.js
+++ src/header/header.js
@@ -3,6 +3,7 @@ import './header.css';
 
 const Header = () => {
     const [isSticky, setIsSticky] = useState(false);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -22,13 +23,18 @@ const Header = () => {
                 <div className="logo-box">
                     {/* Logo here */}
                 </div>
-                <nav className="navbar">
+                <div className={`hamburger ${isMobileMenuOpen ? 'open' : 'closed'}`} onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <nav className={`navbar ${isMobileMenuOpen ? 'open' : ''}`}>
                     <ul>
                         <li><a href="/">Home</a></li>
                         <li><a href="/bot">Bot</a></li>
                         <li><a href="/homelab">Homelab</a></li>
                         <li><a href="">Eternal Horizon</a></li>
-                        <li><a href="">contact</a></li>
+                        <li><a href="">Contact</a></li>
                     </ul>
                 </nav>
             </div>
