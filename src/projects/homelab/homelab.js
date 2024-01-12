52b1daa0b6ed73ce841c1b3147cc7a6df4bc43f3
mahajar2 <rahul.mahajan2@mail.dcu.ie>
2024-01-12 18:55:58 +0000

    Added more content to site

--- src/projects/homelab/homelab.js
+++ src/projects/homelab/homelab.js
@@ -3,7 +3,7 @@ import React from 'react';
 import homelabLogo from '../../images/homelab.png';
 import Header from '../../header/header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHammer, faComments, faPlay, faPause, faForward, faList, faVolumeLow, faCirclePlay, faLayerGroup, faSquarePollVertical, faLanguage, faLink, faTrash, faBroom } from '@fortawesome/free-solid-svg-icons';
+import { faHammer, faComments, faPlay, faPause, faForward, faList, faVolumeLow, faCirclePlay, faLayerGroup, faSquarePollVertical, faLanguage, faLink, faTrash, faBroom, faCloud, faVault, faLaptopFile } from '@fortawesome/free-solid-svg-icons';
 import Footer from '../../footer/Footer';
 
 function Homelab() {
@@ -20,20 +20,52 @@ function Homelab() {
             </div>
             <div className="additional-text two-paragraphs">
                 <div className="paragraph">
+                    <span class="indra-title">Indra - A NextCloud Service</span>
                     <ul>
-                        <li><FontAwesomeIcon icon={faHammer} />Prometheus</li>
+                        <li><FontAwesomeIcon icon={faCloud} />Service to store and access important documents</li>
+                        <li><FontAwesomeIcon icon={faVault} />Safe and Secure</li>
+                        <li><FontAwesomeIcon icon={faLaptopFile} />Can be accessed from anywhere</li>
+                        <div className="homelab-button"><span><a href="" style={{ "textDecoration": "none", "color": "white" }}>Visit Indra</a></span>
+                            <svg>
+                                <polyline class="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                                <polyline class="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                            </svg>
+                        </div>
                     </ul>
                 </div>
                 <div className="paragraph">
                     <ul>
-                        <li><FontAwesomeIcon icon={faCirclePlay} />Plex</li>
+                        <li><FontAwesomeIcon icon={faCirclePlay} />Plex - Media Hosting</li>
+                        <div className="homelab-button"><span><a href="" style={{ "textDecoration": "none", "color": "white" }}>Visit Plex</a></span>
+                            <svg>
+                                <polyline class="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                                <polyline class="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                            </svg>
+                        </div>
                     </ul>
                 </div>
             </div>
             <div className="additional-text two-paragraphs">
                 <div className="paragraph">
                     <ul>
+                        <li><FontAwesomeIcon icon={faHammer} />Prometheus</li>
+                        <div className="homelab-button"><span><a href="" style={{ "textDecoration": "none", "color": "white" }}>Visit Prometheus</a></span>
+                            <svg>
+                                <polyline class="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                                <polyline class="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                            </svg>
+                        </div>
+                    </ul>
+                </div>
+                <div className="paragraph">
+                    <ul>
                         <li><FontAwesomeIcon icon={faLayerGroup} />Home Assistant</li>
+                        <div className="homelab-button"><span><a href="" style={{ "textDecoration": "none", "color": "white" }}>Visit Home Assistant</a></span>
+                            <svg>
+                                <polyline class="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                                <polyline class="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                            </svg>
+                        </div>
                     </ul>
                 </div>
             </div>
