9ec6c5dda433a8ba13ad2a25fac3c22a4899b439
mahajar2 <rahul.mahajan2@mail.dcu.ie>
2024-01-16 22:04:35 +0000

    Added blackjack details to discord bot

--- src/projects/discord_bot/bot.js
+++ src/projects/discord_bot/bot.js
@@ -3,8 +3,9 @@ import './bot.css';
 import discordLogo from '../../images/discord.png';
 import Header from '../../header/header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHammer, faComments, faPlay, faPause, faForward, faList, faVolumeLow, faCirclePlay, faLayerGroup, faSquarePollVertical, faLanguage, faLink, faTrash, faBroom, faBell, faDiamond, faHeart, faUsersBetweenLines, faMoneyBill, faUserGroup, faPenToSquare, faRankingStar, faChartSimple, faListUl } from '@fortawesome/free-solid-svg-icons';
+import { faHammer, faComments, faPlay, faPause, faForward, faList, faVolumeLow, faCirclePlay, faLayerGroup, faSquarePollVertical, faLanguage, faLink, faTrash, faBroom, faBell, faDiamond, faHeart, faUsersBetweenLines, faMoneyBill, faUserGroup, faPenToSquare, faRankingStar, faChartSimple, faListUl, faScaleBalanced, faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
 import Footer from '../../footer/Footer';
+import { faUikit } from '@fortawesome/free-brands-svg-icons';
 
 const Bot = () => {
     return (
@@ -44,13 +45,13 @@ const Bot = () => {
             </div>
             <div className="additional-text bot-two-paragraphs">
                 <div className="bot-paragraph">
-                    <span class="bot-feature-title">Quality of Life</span>
+                    <span class="bot-feature-title">Blackjack</span>
                     <ul>
-                        <li><FontAwesomeIcon icon={faLayerGroup} />Leveling system</li>
-                        <li><FontAwesomeIcon icon={faSquarePollVertical} />User defined polls</li>
-                        <li><FontAwesomeIcon icon={faLanguage} />Translation of Text</li>
-                        <li><FontAwesomeIcon icon={faLink} />Shortening URL</li>
-                        <li><FontAwesomeIcon icon={faBell} />Set a Reminder</li>
+                        <li><FontAwesomeIcon icon={faScaleBalanced} />Standard Rules</li>
+                        <li><FontAwesomeIcon icon={faPeopleGroup} />Player vs Player vs Dealer</li>
+                        <li><FontAwesomeIcon icon={faMoneyBill} /> Accepts bets using virtual(fake) currency</li>
+                        <li><FontAwesomeIcon icon={faRankingStar} />Leaderboard and Rankings</li>
+                        <li><FontAwesomeIcon icon={faUikit} />Well Built UI using Embeds</li>
                     </ul>
                 </div>
                 <div className="bot-paragraph">
@@ -61,6 +62,19 @@ const Bot = () => {
                         <li><FontAwesomeIcon icon={faRankingStar} />Leaderboard and Rankings</li>
                         <li><FontAwesomeIcon icon={faChartSimple} />Individual Statistics</li>
                         <li><FontAwesomeIcon icon={faListUl} />- (ToDo) Add variants like Omaha and Seven-Card Stud</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="additional-text bot-two-paragraphs">
+                <div className="bot-paragraph">
+                    <span class="bot-feature-title">Quality of Life</span>
+                    <ul>
+                        <li><FontAwesomeIcon icon={faLayerGroup} />Leveling system</li>
+                        <li><FontAwesomeIcon icon={faSquarePollVertical} />User defined polls</li>
+                        <li><FontAwesomeIcon icon={faLanguage} />Translation of Text</li>
+                        <li><FontAwesomeIcon icon={faLink} />Shortening URL</li>
+                        <li><FontAwesomeIcon icon={faBell} />Set a Reminder</li>
                     </ul>
                 </div>
             </div>
